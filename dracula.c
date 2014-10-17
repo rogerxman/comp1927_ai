466c7d72b73b2f193c4e94300e9585dabd30e3c2
Roger Tsang <zenghanzhang46@gmail.com>
2014-10-17 17:03:56 +1100

    10/17/2014

--- dracula.c
+++ dracula.c
@@ -5,11 +5,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
+//#include <time.h>
 #include "Game.h"
 #include "Map.h"
 #include "DracView.h"
 #include "Queue.h"
+
+//#define DEBUG
 
 #define SIZE_TAUNT MESSAGE_SIZE
 #define NUM_HUNTER 4
@@ -17,15 +19,17 @@
 //UPDATE: ALPHA
 static LocationID drac_nextLocation(DracView gameState, LocationID destination);
 static LocationID drac_idealPlace(DracView gameState);
-static LocationID drac_randomPlace(DracView gameState);
+//static LocationID drac_randomPlace(DracView gameState);
 static LocationID drac_autoNextMove(DracView gameState);
 static int drac_duplicated(LocationID trail[TRAIL_SIZE]);
 static int drac_isLegalMove(DracView gameState, LocationID destination);
 static void map_howManyStep(DracView gameState, int steps[NUM_MAP_LOCATIONS]);
 
+//UPDATE: SHIT THAT BOAT AND SEA, FREAKING CONFUSING!
+
 void decideDraculaMove(DracView gameState)
 {
-   srand(time(NULL));
+   //srand(time(NULL));
    //0   Step: List out frequently used variable;
    Round game_round = giveMeTheRound(gameState);
 
@@ -39,8 +43,8 @@ void decideDraculaMove(DracView gameState)
       placeToGo = drac_autoNextMove(gameState);
    }
 
-	// Replace the line below by something better
-	registerBestPlay(idToabbrev(placeToGo), "LarDracula Alpha!");
+   // Replace the line below by something better
+   registerBestPlay(idToabbrev(placeToGo), "10/17/2014 4:39PM");
    #ifdef DEBUG
    LocationID trail[TRAIL_SIZE];
    giveMeTheTrail(gameState, PLAYER_DRACULA, trail);
@@ -79,26 +83,48 @@ static LocationID drac_idealPlace(DracView gameState) {
    return idealPlace;
 }
 
-static LocationID drac_randomPlace(DracView gameState) {
+/*static LocationID drac_randomPlace(DracView gameState) {
    LocationID *places;
    int numPlaces, numRandom;
    places = whereCanIgo(gameState, &numPlaces, TRUE, FALSE);
    numRandom = rand() % numPlaces;
    return places[numRandom];
 }
-
+*/
 static LocationID drac_autoNextMove(DracView gameState) {
+   LocationID myLocation = whereIs(gameState, PLAYER_DRACULA);
    LocationID idealDes = drac_nextLocation(gameState, drac_idealPlace(gameState));
-   LocationID finalDes = idealDes;
+   LocationID finalDes;
+   LocationID *placeICanReach = malloc(sizeof(LocationID) * NUM_MAP_LOCATIONS);
+   int numLocations, i, isLegal = FALSE;
    #ifdef DEBUG
-   printf("idealDes = %d\n", idealDes);
-   printf("isLegal  = %d\n", drac_isLegalMove(gameState, idealDes));
+   printf("idealPlace = %d\n", idealDes);
+   printf("ideal_legal? = %d\n", drac_isLegalMove(gameState, idealDes));
    #endif
    if (drac_isLegalMove(gameState, idealDes)) {
+      finalDes = idealDes;
    } else {
-      while((finalDes = drac_randomPlace(gameState)) &&
-             !drac_isLegalMove(gameState, finalDes));
+      //I don't want to go to the sea for some reason!
+      Drac_whereCanIGo(placeICanReach, &numLocations, myLocation, TRUE, FALSE);
+      for (i = 0; i < numLocations && isLegal == FALSE; i++) {
+         finalDes = placeICanReach[i];
+         isLegal = drac_isLegalMove(gameState, finalDes);
+      }
+      //But if i have to go, then i go for it!
+      if (isLegal == FALSE) {
+         Drac_whereCanIGo(placeICanReach, &numLocations, myLocation, TRUE, TRUE);
+         for (i = 0; i < numLocations && isLegal == FALSE; i++) {
+            finalDes = placeICanReach[i];
+            printf("%d -> ", finalDes);
+            isLegal = drac_isLegalMove(gameState, finalDes);
+         }
+      }
+   #ifdef DEBUG
+   printf("finalPlace = %d\n", finalDes);
+   printf("final_legal? = %d\n", drac_isLegalMove(gameState, finalDes));
+   #endif
    }
+   free(placeICanReach);
    return finalDes;
 }
 
@@ -106,7 +132,7 @@ static int drac_duplicated(LocationID trail[TRAIL_SIZE]) {
    int i, j, duplicate = 0;
    for (i = 0; i < TRAIL_SIZE-1; i++) {
       for (j = i + 1; j < TRAIL_SIZE; j++) {
-         if (trail[i] == trail[j] && trail[i] != -1 && trail[j] != -1) {
+         if (trail[i] != -1 && trail[j] != -1 && trail[i] == trail[j]) {
             duplicate++;
          }
       }
@@ -129,19 +155,13 @@ static int drac_isLegalMove(DracView gameState, LocationID destination) {
    if (drac_duplicated(trail) == 0) {
       isLegal = TRUE;
    } else {
-      
       isLegal = FALSE;
    }
    #ifdef DEBUG
-   for (i = 0; i < TRAIL_SIZE; i++) {
-      printf("[%d]\t",trail[i]);
-   }
    printf("\nisLegal = %d\n", isLegal);
    #endif
    return isLegal;
 }
-
-
 
 static void map_howManyStep(DracView gameState, int steps[NUM_MAP_LOCATIONS]) {
    LocationID hunterLocation[NUM_HUNTER];
--- hunter.c
+++ hunter.c
@@ -6,56 +6,70 @@
 #include "Game.h"
 #include "HunterView.h"
 #include <time.h>
-static char* doubleBack(int i, LocationID trail[], HunterView gameState);
-static char* randomCity(HunterView gameState);
+static char *doubleBack(int i, LocationID trail[], HunterView gameState);
+static char *randomCity(HunterView gameState);
 //static void  print(int *a);
 
-void decideHunterMove(HunterView gameState)
-{
+void decideHunterMove(HunterView gameState) {
     LocationID trail[TRAIL_SIZE];
-    giveMeTheTrail(gameState,PLAYER_DRACULA,trail);
-    int i = 0;
-    PlayerID me = whoAmI(gameState);
-    LocationID now = whereIs(gameState,me);
-    if (now == UNKNOWN_LOCATION){
-       switch (me){
-          case 0: registerBestPlay("LO","I'm on holiday in Geneva");break;
-          case 1: registerBestPlay("TO","I'm on holiday in Geneva");break;
-          case 2: registerBestPlay("MU","I'm on holiday in Geneva");break;
-          case 3: registerBestPlay("SO","I'm on holiday in Geneva");break;
-       }
-    }  else{
-       char * destination;
-       while (trail[i] == CITY_UNKNOWN || trail[i] == SEA_UNKNOWN || trail[i] == UNKNOWN_LOCATION) i++;
-       if ( i < TRAIL_SIZE ){
-          if (trail[i] <= MAX_MAP_LOCATION && trail[i] >= MIN_MAP_LOCATION){
-             LocationID path[NUM_MAP_LOCATIONS];
-             efficientPath (gameState,path,trail[i]);
-             int ID = path[1];
-             destination = idToabbrev(ID);
-             registerBestPlay(destination,"I'm on holiday in Geneva");
-          }  else{
-             switch (trail[i]){
-                case DOUBLE_BACK_1: i++; destination = doubleBack(i,trail,gameState);break;
-                case DOUBLE_BACK_2: i+=2; destination = doubleBack(i,trail,gameState);break;
-                case DOUBLE_BACK_3: i+=3; destination = doubleBack(i,trail,gameState);break;
-                case DOUBLE_BACK_4: i+=4; destination = doubleBack(i,trail,gameState);break;
-                case DOUBLE_BACK_5: i+=5; destination = doubleBack(i,trail,gameState);break;
-                default : destination = randomCity(gameState);
-             }
-          }
-       }  else {
-          destination = randomCity(gameState);
-          registerBestPlay(destination,"I'm on holiday in Geneva");
-       }
-   }         
+    giveMeTheTrail(gameState, PLAYER_DRACULA, trail);
+    PlayerID hunterId = whoAmI(gameState);
+    LocationID currentLocation = whereIs(gameState, hunterId);
+
+    // Game just started... get a starting position
+    if (currentLocation == UNKNOWN_LOCATION) {
+        switch (hunterId) {
+            case 0: registerBestPlay("LO","I'm on holiday in Geneva"); break;
+            case 1: registerBestPlay("TO","I'm on holiday in Geneva"); break;
+            case 2: registerBestPlay("MU","I'm on holiday in Geneva"); break;
+            case 3: registerBestPlay("SO","I'm on holiday in Geneva"); break;
+        }
+    } else {
+        // Check if we have found dracula's trail
+        char *destination;
+        int known = -1;
+        int i = 0;
+        // Look for one of dracula's trails
+        for (i = 0; i < TRAIL_SIZE; i++) {
+            if (trail[i] != CITY_UNKNOWN && trail[i] != SEA_UNKNOWN && trail[i] != UNKNOWN_LOCATION) {
+                known = i;
+                break;
+            }
+        }
+        if (known != -1) {
+            // Found one of dracula's trails
+            if (trail[known] <= MAX_MAP_LOCATION && trail[known] >= MIN_MAP_LOCATION){
+                // It's a location... follow him!
+                LocationID path[NUM_MAP_LOCATIONS] = {0};
+                efficientPath (gameState,path,trail[known]);
+                int ID = path[1];
+                destination = idToabbrev(ID);
+            } else {
+                // It's a doubleback!
+                switch (trail[known]){
+                    case DOUBLE_BACK_1: known++; destination = doubleBack(known, trail, gameState);break;
+                    case DOUBLE_BACK_2: known+=2; destination = doubleBack(known, trail, gameState);break;
+                    case DOUBLE_BACK_3: known+=3; destination = doubleBack(known, trail, gameState);break;
+                    case DOUBLE_BACK_4: known+=4; destination = doubleBack(known, trail, gameState);break;
+                    case DOUBLE_BACK_5: known+=5; destination = doubleBack(known, trail, gameState);break;
+                    default : destination = randomCity(gameState);
+                }
+            }
+        } else {
+            // Cannot find dracula, move to random city
+            destination = randomCity(gameState);
+        }
+
+        // Register move
+        registerBestPlay(destination,"I'm on holiday in Geneva");
+    }         
 }
 
-static char* doubleBack(int i, LocationID trail[],HunterView gameState){
+static char *doubleBack(int i, LocationID trail[],HunterView gameState) {
    char * destination;
    if (i >= TRAIL_SIZE){
       destination = randomCity(gameState);
-   }  else {
+   } else {
       if (trail[i] >= MIN_MAP_LOCATION && trail[i] <= MAX_MAP_LOCATION){
          destination = idToabbrev(trail[i]);
       }  else{
@@ -65,54 +79,35 @@ static char* doubleBack(int i, LocationID trail[],HunterView gameState){
    return destination;
 }
 
-static char* randomCity(HunterView gameState){
-   int  num;
-   int numa = 0;
-   int numb = 0;
-   int numc = 0;
-   int visited[NUM_MAP_LOCATIONS] = {0};
-   LocationID* a = whereCanIgo(gameState,&numa,1,0,0);
-   LocationID* b = whereCanIgo(gameState,&numb,0,1,0);
-   LocationID* c = whereCanIgo(gameState,&numc,0,0,1);
-   num = numa + numb + numc;
-   LocationID combine[NUM_MAP_LOCATIONS] = {0};
-   int i;
-   numa--;
-   numb--;
-   numc--;
-   for  (i = 0; numa >= 0; i++){
-   	    if (!visited[a[numa]]){
-       	   combine[i] = a[numa--];
-       	   visited[combine[i]] = 1;
-        }else {
-       	   numa--;
-       	   i--;
+static char *randomCity(HunterView gameState) {
+    int numRandomRoad = 0;
+    int numRandomRail = 0;
+    int numRandomSea = 0;
+    PlayerID player = whoAmI(gameState);
+    LocationID *randomRoads = whereCanPlayerGo(gameState, &numRandomRoad, player, 1, 0, 0);
+    LocationID *randomRails = whereCanPlayerGo(gameState, &numRandomRail, player, 0, 1, 0);
+    LocationID *randomSeas = whereCanPlayerGo(gameState, &numRandomSea, player, 0, 0, 1);
+
+    int numTotalRandom = numRandomRoad + numRandomRail + numRandomSea;
+    LocationID randomCities[NUM_MAP_LOCATIONS] = {0};
+    int i;
+    for (i = 0; i < numTotalRandom; i++) {
+        if (numRandomRoad > 0) {
+            randomCities[i] = randomRoads[--numRandomRoad];
+            //printf("[ROAD] Added %s to %d\n", idToName(randomRoads[numRandomRoad]), i);
+        } else if (numRandomRail > 0) {
+            randomCities[i] = randomRails[--numRandomRail];
+            //printf("[RAIL] Added %s to %d\n", idToName(randomRails[numRandomRail]), i);
+        } else if (numRandomSea > 0) {
+            randomCities[i] = randomSeas[--numRandomSea];
+            //printf("[SEAS] Added %s to %d\n", idToName(randomSeas[numRandomSea]), i);
         }
-   }
-   for (; numb >= 0; i++){
-        if (!visited[b[numb]]){
-       	   combine[i] = b[numb--];
-       	   visited[combine[i]] = 1;
-        }else {
-       	   numb--;
-       	   i--;
-        }
-   }
-   for (; numc >= 0; i++){
-        if (!visited[c[numc]]){
-       	   combine[i] = c[numc--];
-       	   visited[combine[i]] = 1;
-        }else {
-       	   numc--;
-       	   i--;
-        }
-   }
-  //print(combine);
-   srand(time(NULL));
-   int r = rand();
-   int R = r%num;
-   char *tobereturn = idToabbrev(combine[R]);
-   return tobereturn;
+    }
+
+    srand(time(NULL));
+    int random = rand()%numTotalRandom;
+    char *selectedCity = idToabbrev(randomCities[random]);
+    return selectedCity;
 }
 
 /*static void  print (int *a){
